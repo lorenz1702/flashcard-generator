b351478e33ae59d7caa1d1df6f5e5b55be19a195
lorenz1702 <112474057+lorenz1702@users.noreply.github.com>
2025-09-25 16:51:10 +0200

    Update and rename FlashcardData.kt to AnkiExportResult.kt

---
+++ src/main/kotlin/domain/AnkiExportResult.kt
@@ -0,0 +1,5 @@
+package org.example.domain
+
+data class AnkiExportResult(
+    val notesContent: List<String>,       // Die konvertierten Zeilen für Anki
+)
--- src/main/kotlin/domain/FlashcardData.kt
+++
@@ -1,7 +0,0 @@
-package org.example.domain
-
-data class FlashcardData(
-    val originalSentence: Sentence,
-    val TranslatedSentnce: Translation,
-    val audiofile: Audio
-)
